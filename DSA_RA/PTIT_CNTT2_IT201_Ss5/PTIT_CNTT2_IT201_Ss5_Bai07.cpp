431210d5d08ce80dd4b716ffdef14add42527736
Quynh181226 <quynh2682@icloud.com>
2025-06-27 09:55:18 +0700

    DSA

--- DSA_RA/PTIT_CNTT2_IT201_Ss5/PTIT_CNTT2_IT201_Ss5_Bai07.cpp
+++ DSA_RA/PTIT_CNTT2_IT201_Ss5/PTIT_CNTT2_IT201_Ss5_Bai07.cpp
@@ -1,22 +1,23 @@
 //n so fibo dau tien
 #include <bits/stdc++.h>
 using namespace std;
-long long fibo(int n) {
-    if (n <= 1) return n;
-    return fibo(n - 1) + fibo(n - 2);
+
+long long fibo(int n, int i, int f1, int f2) {
+    if (i>=n) return 0;
+    int fn=f1+f2;
+    // f2=f1;
+    // f1=fn;
+    printf("%d ", fn);
+    return fibo(n,i+1, f2, fn);
 }
-long long fibo1(int n) {
-    if (n<=1) return n;
-    //vector<long long> dp(n+1, 0);
-    long long dp[n+1];
-    dp[0] = 1;
-    dp[1] = 1;
-    for (int i = 2; i <= n; i++) dp[i] = dp[i-1] + dp[i-2];
-    //fibo of n
-    return dp[n];
-}
+
 main() {
     int n; cin >> n;
-    cout << fibo(n) << endl;
-    cout << fibo1(n) << endl;
+    if (n<=0) {
+        printf("Invalid");
+        return 0;
+    }
+    if (n>=1) printf("0 ");
+    if (n>=2) printf("1 ");
+    fibo(n,2, 0, 1);
 }
