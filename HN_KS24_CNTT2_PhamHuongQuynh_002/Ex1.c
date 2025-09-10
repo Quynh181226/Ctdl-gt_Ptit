811f03d27bfd71573be9582c4e766415f6619c4f
Quynh181226 <quynh2682@icloud.com>
2025-09-10 16:09:00 +0700

    DSA

--- HN_KS24_CNTT2_PhamHuongQuynh_002/Ex1.c
+++ HN_KS24_CNTT2_PhamHuongQuynh_002/Ex1.c
@@ -1,91 +1,164 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include <stdlib.h>
 
-typedef struct Node{
-     char name[50];
-      struct Node* next;
-}Node;
+typedef struct Node {
+    char name[50];
+    struct Node* next;
+} Node;
+
 typedef struct Stack {
-     Node* top;
-}Stack;
-Node* createNode(char *name) {
-     if (head == NULL) return NULL;
-     Node* newNode = (Node*)malloc(sizeof(Node));
-     Node* temp = newNode;
-     newNode=temp->next;
-}
-int empty(Stack* s) {
-     return s->top==-1;
+    Node* top;
+} Stack;
+
+Node* createNode(const char *name) {
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode != NULL) {
+        strcpy(newNode->name, name);
+        newNode->next = NULL;
+    }
+    return newNode;
 }
 
-Node* paste() {
-     Node* head = createNode(head);
-     head = head->next;
-     return head;
-}
-Node* copy(Node *head) {
-     if (head == NULL) return NULL;
-     Node* newNode = createNode(head->next);
-     head->next->next = newNode;
-     return newNode;
-}
-Node* undo(Stack* s) {
-     if (s->top) {
-          s->top = s->top->next;
-     }
-     return s->top;
-}
-void Input(Node* node) {
-     printf("Nhập tên: ");
-}
-void print(Node* head) {
-     while (tmp) {
-          head = head->next;
-     }
-     return head;
+Stack *newStack() {
+    Stack *newStack = malloc(sizeof(Stack));
+    if (newStack != NULL) {
+        newStack->top = NULL;
+    }
+    return newStack;
 }
 
+void push(Stack *s, const char *name) {
+    Node *newNode = createNode(name);
+    if (newNode == NULL) {
+        printf("Push thất bại\n");
+        return;
+    }
+    newNode->next = s->top;
+    s->top = newNode;
+}
 
+char *pop(Stack *s) {
+    if (s->top == NULL) {
+        return NULL;
+    }
+    Node *temp = s->top;
+    char *name = malloc(strlen(temp->name) + 1);
+    if (name != NULL) {
+        strcpy(name, temp->name);
+    }
+    s->top = s->top->next;
+    free(temp);
+    return name;
+}
+
+char* Input(char *s) {
+    printf("Input: ");
+    fgets(s, 50, stdin);
+    s[strlen(s) - 1] = '\0';
+    return s;
+}
+
+void Copy(Stack *s) {
+    if (s == NULL) return;
+    char name[50];
+    Input(name);
+    push(s, name);
+}
+
+void Paste(const Stack *s) {
+    if (s == NULL || s->top == NULL) {
+        printf("Empty\n");
+        return;
+    }
+    printf("%s\n", s->top->name);
+}
+
+void Undo(Stack *s, Stack *redo) {
+    if (s == NULL || s->top == NULL) return;
+    char *popped = pop(s);
+    if (popped != NULL) {
+        push(redo, popped);
+        free(popped);
+    }
+}
+
+void Redo(Stack *s, Stack *redo) {
+    if (redo == NULL || redo->top == NULL) return;
+    char *popped = pop(redo);
+    if (popped != NULL) {
+        push(s, popped);
+        free(popped);
+    }
+}
+
+void Show(const Stack *s) {
+    if (s == NULL || s->top == NULL) {
+        printf("Empty\n");
+        return;
+    }
+    Node *current = s->top;
+    int i = 1;
+    while (current != NULL) {
+        printf("%d. %s\n", i, current->name);
+        i++;
+        current = current->next;
+    }
+}
 
 void Free(Stack *s) {
-     while (s->top != NULL) {
-          Node* tmp=s->top;
-          s->top=s->top->next;
-          free(tmp);
-     }
+    if (s == NULL) return;
+    while (s->top != NULL) {
+        char *tmp = pop(s);
+        free(tmp);
+    }
+    free(s);
 }
 
+int main() {
+    Stack *copy = newStack();
+    Stack *redo = newStack();
+    int choice = 0;
 
-int choice=0;
-Stack* s;
+    do {
+        printf("\n===== CLIPBOARD MANAGER =====\n");
+        printf("1. COPY : Sao chép đoạn văn bản mới\n");
+        printf("2. PASTE: Dán nội dung vừa sao chép\n");
+        printf("3. UNDO : Hoàn tác lệnh sao chép gần nhất\n");
+        printf("4. REDO : Phục hồi lệnh vừa undo\n");
+        printf("5. HIỂN THỊ: In toàn bộ clipboard\n");
+        printf("6. THOÁT\n");
+        printf("Your choiuce: ");
+        scanf("%d", &choice);
+        getchar();
 
-do {
-     printf("—————————— CLIPBOARD MANAGER ————————");
-     printf("1. COPY : Sao chép đoạn văn bản mới (nhập một chuỗi)");
-     printf("2. PASTE: Dán nội dung vừa sao chép");
-     printf("3. UNDO: Hoàn tác lệnh sao chép gần nhất (xóa khỏi clipboardStack)");
-     printf("4. REDO: Phục hồi lệnh vừa undo");
-     printf("5. HIỂN THỊ: In toàn bộ nội dung clipboard (từ mới nhất đến cũ nhất)");
-     printf("6. THOÁT: Kết thúc chương trình");
+        switch (choice) {
+            case 1:
+                Copy(copy);
+                break;
+            case 2:
+                Paste(copy);
+                break;
+            case 3:
+                Undo(copy, redo);
+                break;
+            case 4:
+                Redo(copy, redo);
+                break;
+            case 5:
+                Show(copy);
+                break;
+            case 6:
+                Free(copy);
+                Free(redo);
+                printf("Exit program");
+                break;
+            default:
+                printf("Invalid choice");
+                break;
+        }
+    } while (choice != 6);
 
-     switch (choice) {
-          case 1:
-               copy(s);
-               break;
-          case 2:
-               paste();
-               break;
-          case 3:
-               break;
-          case 4:
-               break;
-          case 5:
-               print();
-               break;
-          case 6:
-               printf("Thoat chuong trinh");
-               break;
-          default:
-               printf("Lua chon khong hop le");
-     }
-} while (choice!==6);
+    return 0;
+}
--- HN_KS24_CNTT2_PhamHuongQuynh_002/Ex2.c
+++ HN_KS24_CNTT2_PhamHuongQuynh_002/Ex2.c
@@ -1,41 +1,152 @@
-typedef struct Contact{
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Contact {
     char name[50];
     char phone[15];
     char email[50];
-}Contact;
+} Contact;
 
-typedef struct TreeNode{
-    Contact contact;
-    struct TreeNode* left, right;
-}TreeNode;
+typedef struct TreeNode {
+    Contact d;
+    struct TreeNode *left, *right;
+} TreeNode;
 
-TreeNode* createTree(){
-    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
-    if(newNode==NULL) return NULL;
-    Node* cmp ctrcpy(newNode);
-}
-int choice=0;
-do{
-    printf("====Quản lý danh bạ====");
-    printf("1. Thêm người vào danh bạ");
-    printf("2. Hiển thị danh bạ theo ABC");
-    printf("3. Tìm người theo tên");
-    printf("4. Xóa người theo tên");
-    printf("5. Thoát");
-    switch(choice){
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            printf("Exit program");
-            break;
-        default:
-            printf("Invalid choice");
-            break;
+TreeNode *createNode(const Contact contact) {
+    TreeNode *newNode = malloc(sizeof(TreeNode));
+    if (newNode != NULL) {
+        strcpy(newNode->d.name, contact.name);
+        strcpy(newNode->d.phone, contact.phone);
+        strcpy(newNode->d.email, contact.email);
+        newNode->left = newNode->right = NULL;
     }
-}while(choice !== 5);
+    return newNode;
+}
+
+TreeNode *addContact(TreeNode *root, const Contact c) {
+    if (root == NULL) return createNode(c);
+
+    if (strcmp(c.name, root->d.name) > 0) {
+        root->right = addContact(root->right, c);
+    } else if (strcmp(c.name, root->d.name) < 0) {
+        root->left = addContact(root->left, c);
+    } else {
+        printf("Tên đã tồn tại!\n");
+    }
+    return root;
+}
+
+char* input(char *s, char *label) {
+    printf("%s: ", label);
+    fgets(s, 50, stdin);
+    s[strlen(s) - 1] = '\0';
+    return s;
+}
+
+void printContact(Contact c) {
+    printf("Name: %s\nPhone: %s\nEmail: %s\n", c.name, c.phone, c.email);
+}
+
+void showContact(const TreeNode *root) {
+    if (root == NULL) return;
+    showContact(root->left);
+    printContact(root->d);
+    showContact(root->right);
+}
+
+TreeNode* findMin(TreeNode* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+void searchName(const TreeNode *root, const char *name) {
+    if (root == NULL) return;
+    int cmp = strcmp(name, root->d.name);
+    if (cmp == 0) {
+        printContact(root->d);
+    } else if (cmp < 0) {
+        searchName(root->left, name);
+    } else {
+        searchName(root->right, name);
+    }
+}
+
+TreeNode *deleName(TreeNode *root, char *name) {
+    if (root == NULL) return root;
+    if (strcmp(name, root->d.name) > 0) {
+        root->right = deleName(root->right, name);
+    } else if (strcmp(name, root->d.name) < 0) {
+        root->left = deleName(root->left, name);
+    } else {
+        if (root->left == NULL) {
+            TreeNode* temp = root->right;
+            free(root);
+            return temp;
+        }
+        if (root->right == NULL) {
+            TreeNode* temp = root->left;
+            free(root);
+            return temp;
+        }
+        TreeNode* min = findMin(root->right);
+        root->d = min->d;
+        root->right = deleName(root->right, min->d.name);
+    }
+    return root;
+}
+
+void Free(TreeNode *t) {
+    if (t == NULL) return;
+    Free(t->left);
+    Free(t->right);
+    free(t);
+}
+
+main() {
+    int choice = 0;
+    TreeNode *contact = NULL;
+    char name[50];
+    do {
+        printf("\n==== Quản lý danh bạ ====\n");
+        printf("1. Thêm người vào danh bạ\n");
+        printf("2. Hiển thị danh bạ theo ABC\n");
+        printf("3. Tìm người theo tên\n");
+        printf("4. Xóa người theo tên\n");
+        printf("5. Thoát\n");
+        printf("Chọn: ");
+        scanf("%d", &choice);
+        getchar();
+        switch(choice) {
+            case 1: {
+                Contact c;
+                input(c.name, "Name");
+                input(c.phone, "Phone");
+                input(c.email, "Email");
+                contact = addContact(contact, c);
+                break;
+            }
+            case 2:
+                showContact(contact);
+                break;
+            case 3:
+                input(name, "Name");
+                searchName(contact, name);
+                break;
+            case 4:
+                input(name, "Name");
+                contact = deleName(contact, name);
+                break;
+            case 5:
+                Free(contact);
+                printf("Exit program");
+                break;
+            default:
+                printf("Invalid choice");
+                break;
+        }
+    } while(choice != 5);
+}
